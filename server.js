a83a511790aaf4f39593446ff0fdecb02c9d15d9
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-03-02 16:16:33 +0100

    add data folder and color module

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// const logger = require('./middleware/logger');
+const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
@@ -29,12 +29,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+  )
 );
 
 //handle unhandled prmise rejections
 process.on('unhandleRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // close server &exit process
   server.close(() => process.exit(1));
 });
