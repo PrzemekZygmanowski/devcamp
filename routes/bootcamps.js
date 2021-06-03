8c27364302fcc79e58cde881fa319d5ede810c94
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-06-03 21:55:00 +0200

    creating express router

---
+++ routes/bootcamps.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // res.json({ name: 'przemek' });
+  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+});
+
+router.get('/:id', (req, res) => {
+  // res.json({ name: 'przemek' });
+  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
+});
+
+router.post('/', (req, res) => {
+  // res.json({ name: 'przemek' });
+  res.status(200).json({ success: true, msg: 'create new bootcamp' });
+});
+
+router.put('/:id', (req, res) => {
+  // res.json({ name: 'przemek' });
+  res
+    .status(200)
+    .json({ success: true, msg: `display bootcamp ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+  // res.json({ name: 'przemek' });
+  res
+    .status(200)
+    .json({ success: true, msg: `delete bootcamp ${req.params.id}` });
+});
+
+module.exports = router;
