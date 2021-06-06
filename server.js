c9622dcd6d978225cc5a36b8a84e9b302320f89d
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-06-06 20:16:17 +0200

    connect with database

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-//route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
+
+//connect to databas
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -21,7 +25,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`error: ${err.message}`);
+  //close server & exit process
+  server.close(() => process.exit(1));
+});
