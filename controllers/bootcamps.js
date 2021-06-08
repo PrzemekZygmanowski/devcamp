be53ffc6f1238f255cf9be59a86d546e1f0cbc5a
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-06-08 16:01:46 +0200

    select and sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,19 +2,47 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
+const { deleteMany } = require('../models/Bootcamp');
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
 
-  let queryStr = JSON.stringify(req.query);
+  //copy req.query
+  const reqQuery = { ...req.query };
 
+  //fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  //loop over removefields and delete them from reqquery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  //create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  //create operatore ($gt,$gte etc.)
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
+  //finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  //select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  //sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  //ececuting the query
   const bootcamps = await query;
 
   res
