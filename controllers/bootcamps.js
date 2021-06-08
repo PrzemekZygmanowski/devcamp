16820aed0ca41b52cb6f24b80862991655aa5606
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-06-08 20:17:33 +0200

    course routes and controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,6 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
-const { deleteMany } = require('../models/Bootcamp');
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,8 +7,15 @@ const {
   deleteBootcamp,
   getBootCampsinRadius,
 } = require('../controllers/bootcamps');
+const { route } = require('./courses');
+
+//include other resource routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
+
+//re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootCampsinRadius);
 
