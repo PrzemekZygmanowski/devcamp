e9b34b91e1a639e754d7ec0ef9db954eeb53f7ff
Zygmanowski, Przemyslaw <przemyslaw.zygmanowski@fujitsu.com>
2021-06-11 21:18:56 +0200

    role authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -19,7 +19,7 @@ const courseRouter = require('./courses');
 const router = express.Router();
 
 // // import protect middleware
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 //re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
@@ -29,14 +29,16 @@ router.route('/radius/:zipcode/:distance').get(getBootCampsinRadius);
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
